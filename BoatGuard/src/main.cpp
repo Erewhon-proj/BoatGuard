1e4e0810c64586d2c9db7d3ea232c27b9245d7c1
jackls24 <giacomo1000@live.it>
2025-03-24 15:40:24 +0100

    fix merge

--- BoatGuard/src/main.cpp
+++ BoatGuard/src/main.cpp
@@ -3,6 +3,32 @@
 #include "ormeggio_etichettati.h"
 #include "esp_sleep.h"
 
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLEAdvertisedDevice.h>
+#include <BLEScan.h>
+#include <ArduinoJson.h>
+#include <Preferences.h>
+#include "BLEUtils.h"
+
+#define SERVICE_UUID "12345678-1234-1234-1234-123456789012"
+#define CHARACTERISTIC_UUID "87654321-4321-4321-4321-210987654321"
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
+
+#define BLE_periferica 1
+
+const char *REQUEST_CONNECTION_MESSAGE = "REQUEST_CONNECTION";
+const char *CONFIRM_CONNECTION_MESSAGE = "CONFIRM_CONNECTION";
+
+extern bool isConfigurated;
+extern bool isNearMe;
+extern bool isConnected;
+
+extern void setup();
+
 #define NUMBER_OF_INPUTS 7
 #define NUMBER_OF_OUTPUTS 1
 #define TENSOR_ARENA_SIZE 2 * 1024
@@ -40,10 +66,52 @@ void aggiornaPosizioneBarca(float deltaTimeSec);
 String criptaMessaggio(const char *messaggio);
 void inviaMessaggioLoRa(const char *msg);
 
+void setCostants()
+{
+    IoTBoard::init_serial();
+    IoTBoard::init_leds();
+    IoTBoard::init_display();
+    BLEDevice::init("Esp32");
+
+    preferences.begin("config", false);
+    if (!preferences.isKey("macBle"))
+    {
+        String mac = BLEDevice::getAddress().toString();
+        preferences.putString("macBle", mac);
+        Serial.print("Salvato MAC BLE: ");
+        Serial.println(mac);
+    }
+    preferences.end();
+
+    // set isConfigurated
+    preferences.begin("config", true);
+    isConfigurated = preferences.isKey("targa");
+    preferences.end();
+
+    // set isNearMe
+    isNearMe = getNearMe();
+}
+
 void setup()
 {
     IoTBoard::init_serial();
     randomSeed(millis());
+
+    Serial.begin(115200);
+    setCostants();
+
+    BLEClient *pClient = bleClient();
+
+    if (isConfigurated)
+    {
+        Serial.println("Informazioni già configurate");
+        // printPreferences();
+    }
+
+    if (!isConnected)
+    {
+        startAdvertising();
+    }
 
     // Caricamento modello
     if (!ml.begin(ormeggio_model))
@@ -271,58 +339,6 @@ void aggiornaPosizioneBarca(float deltaTimeSec)
     Serial.println(posY);
 }
 
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
-#include <BLEAdvertisedDevice.h>
-#include <BLEScan.h>
-#include <ArduinoJson.h>
-#include <Preferences.h>
-#include "BLEUtils.h"
-
-#define SERVICE_UUID "12345678-1234-1234-1234-123456789012"
-#define CHARACTERISTIC_UUID "87654321-4321-4321-4321-210987654321"
-
-BLEServer *pServer = NULL;
-BLECharacteristic *pCharacteristic = NULL;
-
-#define BLE_periferica 1
-
-const char *REQUEST_CONNECTION_MESSAGE = "REQUEST_CONNECTION";
-const char *CONFIRM_CONNECTION_MESSAGE = "CONFIRM_CONNECTION";
-
-extern bool isConfigurated;
-extern bool isNearMe;
-extern bool isConnected;
-
-extern void setup();
-
-void setCostants()
-{
-    IoTBoard::init_serial();
-    IoTBoard::init_leds();
-    IoTBoard::init_display();
-    BLEDevice::init("Esp32");
-
-    preferences.begin("config", false);
-    if (!preferences.isKey("macBle"))
-    {
-        String mac = BLEDevice::getAddress().toString();
-        preferences.putString("macBle", mac);
-        Serial.print("Salvato MAC BLE: ");
-        Serial.println(mac);
-    }
-    preferences.end();
-
-    // set isConfigurated
-    preferences.begin("config", true);
-    isConfigurated = preferences.isKey("targa");
-    preferences.end();
-
-    // set isNearMe
-    isNearMe = getNearMe();
-}
-
 void startAdvertising()
 {
 
@@ -362,43 +378,4 @@ BLEClient *bleClient()
     bleScan->start(5, false); // Scansione per 5 secondi
     // BLEDevice::deinit(false);
     return pClient;
-}
-
-void setup()
-{
-    Serial.begin(115200);
-    setCostants();
-
-    BLEClient *pClient = bleClient();
-
-    if (isConfigurated)
-    {
-        Serial.println("Informazioni già configurate");
-        // printPreferences();
-    }
-
-    if (!isConnected)
-    {
-        startAdvertising();
-    }
-}
-
-void loop()
-{
-
-    /*
-    BLEClient *pClient = bleClient();
-
-    if (isConfigurated)
-    {
-        Serial.println("Informazioni già configurate");
-        // printPreferences();
-    }
-    if (!isConnected)
-    {
-        startAdvertising();
-    }
-
-    delay(200);
-    */
 }
