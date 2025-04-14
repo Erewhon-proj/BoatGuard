a0dc2bae6c025ef7dcb6b1af0757d0c2a11c20ea
jackls24 <giacomo1000@live.it>
2025-04-14 15:23:21 +0200

    fix

--- BoatGuard/src/Crypto/CryptUtils.cpp
+++ BoatGuard/src/Crypto/CryptUtils.cpp
@@ -1,11 +1,24 @@
 #include "CryptoUtils.h"
 #include <iot_board.h>
 
+void xorBuffer(void *data, size_t len, const uint8_t *key, size_t key_len, bool recive)
+{
+    if (recive == true)
+    {
+        Serial.println("xorBuffer/Private Key (string): /6  ");
+        Serial.println((const char *)key);
+    }
 
-void xorBuffer(void* data, size_t len, const uint8_t* key, size_t key_len) {
-    uint8_t* byteData = static_cast<uint8_t*>(data);
-    for (size_t i = 0; i < len; ++i) {
+    uint8_t *byteData = static_cast<uint8_t *>(data);
+    for (size_t i = 0; i < len; ++i)
+    {
         byteData[i] ^= key[i % key_len];
+    }
+
+    if (recive == true)
+    {
+        Serial.println("xorBuffer/Private Key (string): /6  ");
+        Serial.println((const char *)key);
     }
 
     Serial.println("XORed DONE");
